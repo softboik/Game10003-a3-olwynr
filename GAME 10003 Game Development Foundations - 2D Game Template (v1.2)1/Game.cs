1e6d6157c5af2eb30f0686bac3aa7dcb385dfb2e
Olwyn Roberts <000852881@mohawkcollege.ca>
2024-11-11 15:24:23 -0500

    add lines to field (colors still broken)

--- GAME 10003 Game Development Foundations - 2D Game Template (v1.2)1/Game.cs
+++ GAME 10003 Game Development Foundations - 2D Game Template (v1.2)1/Game.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Data;
 using System.Numerics;
+using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
 
 // The namespace your code is in.
@@ -14,10 +15,10 @@ namespace Game10003
     {
         // Variables:
         Color RedTeam = new Color(200, 5, 5);
-
+        
         Color BlueTeam = new Color(0, 160, 220);
 
-        
+       // Draw Rpaddle = new Draw.Capsule(30, 220, 30, 380, 10);
 
         public void Setup()
         {
@@ -31,18 +32,37 @@ namespace Game10003
         {
             Window.ClearBackground(Color.Black);
 
+            Draw.Line(400, 600, 400, 0);
+            Draw.LineColor = Color.Gray;
+
+            Draw.Circle(400, 300, 200);
+            Draw.LineColor = Color.Gray;
+            Draw.FillColor = Color.Black;
+
             Draw.Capsule(30, 220, 30, 380, 10);
+            Draw.FillColor = BlueTeam;
             Draw.LineColor = Color.White;
             Draw.LineSize = 2;
-            Draw.FillColor = BlueTeam;
+            
 
             Draw.Capsule(770, 220, 770, 380, 10);
+            Draw.FillColor = RedTeam;
             Draw.LineColor = Color.White;
             Draw.LineSize = 2;
-            Draw.FillColor = RedTeam;
+            
 
             Draw.Circle(400, 300, 20);
             Draw.FillColor = Color.White;
+
+            if (Input.IsKeyboardKeyDown(KeyboardInput.W))
+            {
+
+            }
+            else if (Input.IsKeyboardKeyDown(KeyboardInput.S))
+            {
+
+            }
+
         }
     }
 }
